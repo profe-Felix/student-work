d2e8de25be3fbf4fbd457e0bcf1684ba07333c68
profe-Felix <josefelix.edu@gmail.com>
2025-09-21 02:58:08 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
-import StudentAssignment from './pages/student/assignment'
 import StartPicker from './pages/start'
-
-const base = import.meta.env.BASE_URL || '/'
+import StudentAssignment from './pages/student/assignment'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,8 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Navigate to="/start" replace />} />
         <Route path="/start" element={<StartPicker />} />
         <Route path="/student/assignment" element={<StudentAssignment />} />
-        {/* Teacher route will come next */}
-        {/* <Route path="/teacher" element={<TeacherDashboard />} /> */}
         <Route path="*" element={<Navigate to="/start" replace />} />
       </Routes>
     </HashRouter>
