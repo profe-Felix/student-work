44db991b21012a394666c9bf377c8d5bbe195bfb
profe-Felix <josefelix.edu@gmail.com>
2025-09-21 01:03:37 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // This is the public base path for GitHub Pages at /student-work/
+  base: process.env.BASE || '/student-work/',
 })
