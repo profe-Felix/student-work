1128580e67ca19b3551d8142df15a7b294a5d268
profe-Felix <josefelix.edu@gmail.com>
2025-09-28 10:28:41 -0500

    Delete src/lib/supabaseHelpers.ts

--- src/lib/supabaseHelpers.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/supabaseHelpers.ts
-import { supabase } from './supabaseClient';
-
-export function publicUrl(path: string) {
-  const { data } = supabase.storage.from('public').getPublicUrl(path);
-  return data.publicUrl;
-}
