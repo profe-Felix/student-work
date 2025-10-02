2115422dc0f62976aa73c0a6c3aa43f440a3c156
profe-Felix <josefelix.edu@gmail.com>
2025-10-02 00:10:05 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/student-work/',
+  resolve: {
+    dedupe: ['react', 'react-dom'], // <- ensure only one copy is bundled
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
