af25e359703ef83890af143f85cd3abf7ca9e50e
profe-Felix <josefelix.edu@gmail.com>
2025-10-02 22:42:44 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // This is the public base path for GitHub Pages at /student-work/
-  base: process.env.BASE || '/student-work/',
-})
