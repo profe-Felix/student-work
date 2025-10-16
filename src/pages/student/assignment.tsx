fd1b56d3ddf516bbaa5f85615b16be4a33fab581
profe-Felix <josefelix.edu@gmail.com>
2025-10-16 04:45:17 -0500

    Update assignment.tsx

--- src/pages/student/assignment.tsx
+++ src/pages/student/assignment.tsx
@@ -739,7 +739,7 @@ export default function StudentAssignment(){
     let detach: (() => void) | null = null
     ;(async () => {
       try { await ensureSaveWorker() } catch {}
-      detach = attachBeforeUnloadSave(async () => buildSavePayload())
+          detach = attachBeforeUnloadSave(async () => buildSavePayload(), {})
     })()
     return () => { try { detach?.() } catch {} }
   }, [studentId, classCode, rtAssignmentId, pageIndex, canvasSize.w, canvasSize.h])
